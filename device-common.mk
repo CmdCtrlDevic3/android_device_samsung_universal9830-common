047ffeaec37c550575678b58b7ccaa15a8a8307a
Tim Zimmermann <tim@linux4.de>
2023-12-02 10:48:06 +0100

    universal9830: Switch to fscrypt v2 for /data
    * Also enable casefolding and disable sdcardfs
    
    Change-Id: I8195121f4836700c3a217d2a66fc98e5fbd5303c
    Signed-off-by: ExtremeXT <extremextdev@gmail.com>

--- device-common.mk
+++ device-common.mk
@@ -16,6 +16,9 @@
 
 COMMON_PATH := device/samsung/universal9830-common
 
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
 # Additional native libraries
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
