4eddf402727113695a09d253b4fc1298fd384077
borbelyvince <borbely.vince.11@gmail.com>
2024-11-10 11:18:18 +0100

    universal9830: shim widevine with libcrypto_shim
    from hardware/lineage/compat
    
    Change-Id: I318fbbc32f850dede2c44bebc232f5da220ef4ab
    Signed-off-by: ExtremeXT <extremextdev@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -85,6 +85,7 @@ function blob_fixup() {
             ;;
         vendor/lib*/libwvhidl.so)
             "${PATCHELF}" --replace-needed libprotobuf-cpp-lite-3.9.1.so libprotobuf-cpp-full-3.9.1.so "${2}"
+            "${PATCHELF}" --add-needed libcrypto_shim.so "${2}"
             ;;
         vendor/lib*/sensors.*.so)
             "${PATCHELF}" --replace-needed libutils.so libutils-v32.so "${2}"
