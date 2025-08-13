5895c32046427a9d184776b5b34356ae9d76f77c
LuK1337 <priv.luk@gmail.com>
2025-08-13 21:56:50 +0200

    universal9830: Migrate to Lineage Health IFastCharge
    Change-Id: Ia2f1f213de2f2e5afd913e5de38712fddc386b49
    Signed-off-by: Ksawlii <maja.blaszczak@proton.me>
    Signed-off-by: ExtremeXT <extremextdev@gmail.com>

--- device-common.mk
+++ device-common.mk
@@ -83,10 +83,6 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_PACKAGES += \
     fastbootd
 
-# FastCharge
-PRODUCT_PACKAGES += \
-    vendor.lineage.fastcharge@1.0-service.samsung
-
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl:64 \
