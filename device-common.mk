e6ab4508e33ac4c506906ecfa21c20cec2f22ec6
CmdCtrlDevic3 <cmdctrldevic3@proton.me>
2025-10-26 10:58:36 +0200

    universal9830: Build secure element from source
    Change-Id: Ife58b6f27fde1c40762be38ed6eca17c170c39d5
    Signed-off-by: CmdCtrlDevic3 <cmdctrldevic3@proton.me>

--- device-common.mk
+++ device-common.mk
@@ -338,7 +338,7 @@ PRODUCT_PACKAGES += \
 
 # Secure Element
 PRODUCT_PACKAGES += \
-    android.hardware.secure_element@1.2.vendor:64
+    android.hardware.secure_element@1.2-service-gto
 
 # Sensors
 PRODUCT_PACKAGES += \
